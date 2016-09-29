39441285687c64dd7d6ddc85e977245b1acef883
ArchITech <swtdavid@gmail.com>
2016-09-29 09:55:03 -0700

    bug fix

--- client/grid.js
+++ client/grid.js
@@ -2,86 +2,91 @@ var DURATION_MIN = 0;
 var DURATION_MAX = 200;
 
 
-// register the grid component
-Vue.component('qtime-grid', {
-  template: '#grid-template',
-  props: {
-    data: Array,
-    columns: Array,
-    filterKey: String
-
-  },
-  data: function () {
-    var sortOrders = {}
-    this.columns.forEach(function (key) {
-      sortOrders[key] = 1
-    })
-    return {
-      sortKey: '',
-      sortOrders: sortOrders,
-      durationMin: 0,
-      durationMax: 99,
-      selectedCategory: [],
-      options: [
-        { text: 'None', value: '' },
-        { text: 'Movie', value: 'movie' },
-        { text: 'Anime', value: 'anime' },
-        { text: 'Show', value: 'show' }
-      ],
-    }
-  },
-  methods: {
-
-    startEdit: function(event) {
-      $(event.target).addClass('editingBackground');
-    },
-
-    cellContentChanged: function (event) {
-      var rowIndex = event.target.dataset.index;
-      var colName = event.target.dataset.name;
-      var val = $(event.target).text();
-      // console.log('rowIndex '+rowIndex );
-      // console.log('colName '+colName);
-      // console.log('innerText '+event.target.innerText);
-      qtime.$data.gridData[rowIndex][colName] = val;
-      $(event.target).text(val); //added this line because of a bug
-      // The bug is when adding a cell that's empty then edit that cell
-      // the value can be saved correctly to gridData, but rendered twice,
-      // so entering 'a' will show 'aa'
-      $(event.target).removeClass('editingBackground');
-
-      
-    },
-
-    sortBy: function (key) {
-      this.sortKey = key
-      this.sortOrders[key] = this.sortOrders[key] * -1
-    },
-
-    filterByDuration: function (entry) {
-      if (isNaN(entry.duration))
-        return true;
-      var durMax = this.durationMax === 'INF' ? 99999 : this.durationMax;
-      return entry.duration >= this.durationMin && entry.duration <= durMax;
-    },
-
-    filterByCategory: function (entry) {
-      // If the entry's category is in the selected categories array, keep the entry
-      var i=0;
-      for (; i<this.selectedCategory.length; i++) {
-
-        if (entry.category === this.selectedCategory[i]||this.selectedCategory[i]==='')
-          return true;
-      }
-
-      if (!i) return true;
-
-      return false;
-    }
-  }
-})
 
 $.get("/api/data", function(jsonData, status){
+
+
+
+
+  // register the grid component
+  Vue.component('qtime-grid', {
+    template: '#grid-template',
+    props: {
+      data: Array,
+      columns: Array,
+      filterKey: String
+
+    },
+    data: function () {
+      var sortOrders = {}
+      this.columns.forEach(function (key) {
+        sortOrders[key] = 1
+      })
+      return {
+        sortKey: '',
+        sortOrders: sortOrders,
+        durationMin: 0,
+        durationMax: 99,
+        selectedCategory: [],
+        options: [
+          { text: 'None', value: '' },
+          { text: 'Movie', value: 'movie' },
+          { text: 'Anime', value: 'anime' },
+          { text: 'Show', value: 'show' }
+        ],
+      }
+    },
+    methods: {
+
+      startEdit: function(event) {
+        $(event.target).addClass('editingBackground');
+      },
+
+      cellContentChanged: function (event) {
+        var rowIndex = event.target.dataset.index;
+        var colName = event.target.dataset.name;
+        var val = $(event.target).text();
+        // console.log('rowIndex '+rowIndex );
+        // console.log('colName '+colName);
+        // console.log('innerText '+event.target.innerText);
+        qtime.$data.gridData[rowIndex][colName] = val;
+        $(event.target).text(val); //added this line because of a bug
+        // The bug is when adding a cell that's empty then edit that cell
+        // the value can be saved correctly to gridData, but rendered twice,
+        // so entering 'a' will show 'aa'
+        $(event.target).removeClass('editingBackground');
+
+        
+      },
+
+      sortBy: function (key) {
+        this.sortKey = key
+        this.sortOrders[key] = this.sortOrders[key] * -1
+      },
+
+      filterByDuration: function (entry) {
+        if (isNaN(entry.duration))
+          return true;
+        var durMax = this.durationMax === 'INF' ? 99999 : this.durationMax;
+        return entry.duration >= this.durationMin && entry.duration <= durMax;
+      },
+
+      filterByCategory: function (entry) {
+        // If the entry's category is in the selected categories array, keep the entry
+        var i=0;
+        for (; i<this.selectedCategory.length; i++) {
+
+          if (entry.category === this.selectedCategory[i]||this.selectedCategory[i]==='')
+            return true;
+        }
+
+        if (!i) return true;
+
+        return false;
+      }
+    }
+  })
+
 
   // bootstrap the qtime
   var qtime = new Vue({
