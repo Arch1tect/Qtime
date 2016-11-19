b173139f8d24ea41b6ae50e29ffa1023bf7f7195
ArchITech <swtdavid@gmail.com>
2016-11-19 12:22:50 -0800

    use port 9000

--- src/server/config/config.py
+++ src/server/config/config.py
@@ -1,10 +1,10 @@
 import os
 
-port = 80
+port = 9000
 
-if 'SWT_GEO' in os.environ and os.environ['SWT_GEO'] == 'ASIA':
-	port = 80
-	print "Running in Asian server"
-else:
-	port = 80
-	print "Running in non-Asian server"
+# if 'SWT_GEO' in os.environ and os.environ['SWT_GEO'] == 'ASIA':
+# 	port = 80
+# 	print "Running in Asian server"
+# else:
+# 	port = 80
+# 	print "Running in non-Asian server"
